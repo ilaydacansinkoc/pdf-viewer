2683d1eaac76e9cc0f52dddf3eda6c195770b299
= <=>
2019-12-27 19:57:38 +0300

    app.tsx edits. use component 2 if you want to see stylized pdf.,

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import PdfViewerOne from "./components/PdfViewerOne";
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import PdfViewerTwo from "./components/PdfViewerTwo";
+
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <PdfViewerOne/>
+      {/*<PdfViewerOne>*/} {/* Use PdfViewerOne if you want to see no styling applied.*/}
+      <PdfViewerTwo/> {/* Use PdfViewerTwo if you want to see some styles applied. However background-color and
+                          images cannot be applied by using only plain jspdf library. */}
     </div>
   );
 };
