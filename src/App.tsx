5a059d75bf7ca49758ca92f624eb8c10582ac488
= <=>
2019-12-27 23:22:49 +0300

    PdfViewerThree component added with an image and also some texts retrieved from a form field.

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,16 @@ import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import PdfViewerTwo from "./components/PdfViewerTwo";
+import PdfViewerThree from "./components/PdfViewerThree";
 
 
 const App: React.FC = () => {
   return (
     <div className="App">
       {/*<PdfViewerOne>*/} {/* Use PdfViewerOne if you want to see no styling applied.*/}
-      <PdfViewerTwo/> {/* Use PdfViewerTwo if you want to see some styles applied. However background-color and
+      {/*<PdfViewerTwo/>*/} {/* Use PdfViewerTwo if you want to see some styles applied. However background-color and
                           images cannot be applied by using only plain jspdf library. */}
+      <PdfViewerThree/> {/* Use PdfViewerThree if you want to see a pdf produced with a form and an image.*/}
     </div>
   );
 };
